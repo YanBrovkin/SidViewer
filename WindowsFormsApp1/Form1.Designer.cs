ab4f2d5139737b21d0ffaa240a6e5dc259a81514
Yan Brovkin <brovkin.yan@gmail.com>
2021-03-22 17:19:12 +0200

    feat: add realisation

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourceSidText = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ToBinaryFormButton = new System.Windows.Forms.Button();
+            this.ToStringFormButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ResultText = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sourceSidText
+            // 
+            this.sourceSidText.Location = new System.Drawing.Point(16, 21);
+            this.sourceSidText.Multiline = true;
+            this.sourceSidText.Name = "sourceSidText";
+            this.sourceSidText.Size = new System.Drawing.Size(398, 97);
+            this.sourceSidText.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.sourceSidText);
+            this.groupBox1.Location = new System.Drawing.Point(30, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(431, 138);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Source SID";
+            // 
+            // ToBinaryFormButton
+            // 
+            this.ToBinaryFormButton.Location = new System.Drawing.Point(46, 184);
+            this.ToBinaryFormButton.Name = "ToBinaryFormButton";
+            this.ToBinaryFormButton.Size = new System.Drawing.Size(115, 33);
+            this.ToBinaryFormButton.TabIndex = 2;
+            this.ToBinaryFormButton.Text = "To Binary";
+            this.ToBinaryFormButton.UseVisualStyleBackColor = true;
+            this.ToBinaryFormButton.Click += new System.EventHandler(this.ToBinaryFormButton_Click);
+            // 
+            // ToStringFormButton
+            // 
+            this.ToStringFormButton.Location = new System.Drawing.Point(176, 184);
+            this.ToStringFormButton.Name = "ToStringFormButton";
+            this.ToStringFormButton.Size = new System.Drawing.Size(116, 33);
+            this.ToStringFormButton.TabIndex = 3;
+            this.ToStringFormButton.Text = "To String";
+            this.ToStringFormButton.UseVisualStyleBackColor = true;
+            this.ToStringFormButton.Click += new System.EventHandler(this.ToStringFormButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(346, 184);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(114, 32);
+            this.ClearButton.TabIndex = 4;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ResultText);
+            this.groupBox2.Location = new System.Drawing.Point(30, 253);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(431, 161);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Result";
+            // 
+            // ResultText
+            // 
+            this.ResultText.Location = new System.Drawing.Point(16, 23);
+            this.ResultText.Multiline = true;
+            this.ResultText.Name = "ResultText";
+            this.ResultText.Size = new System.Drawing.Size(398, 117);
+            this.ResultText.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 428);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.ToStringFormButton);
+            this.Controls.Add(this.ToBinaryFormButton);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sourceSidText;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button ToBinaryFormButton;
+        private System.Windows.Forms.Button ToStringFormButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox ResultText;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ToBinaryFormButton_Click(object sender, EventArgs e)
+        {
+            var securityIdentifier = new SecurityIdentifier(this.sourceSidText.Text);
+            var bytes = new byte[securityIdentifier.BinaryLength];
+            securityIdentifier.GetBinaryForm(bytes, 0);
+            //this.ResultText.Text = ConvertBytesToStringSid(bytes);
+            this.ResultText.Text = ByteArrayToVarBinaryString(bytes);
+        }
+
+        private void ToStringFormButton_Click(object sender, EventArgs e)
+        {
+            var bytes = StringVarBinaryToByteArray(this.sourceSidText.Text);
+            var securityIdentifier = new SecurityIdentifier(bytes, 0);
+            this.ResultText.Text = securityIdentifier.ToString();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            this.sourceSidText.Clear();
+            this.ResultText.Clear();
+        }
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool ConvertSidToStringSid(IntPtr Sid,ref IntPtr StringSid);
+
+        [DllImport("advapi32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool ConvertSidToStringSid([MarshalAs(UnmanagedType.LPArray)] byte[] pSID, out IntPtr ptrSid);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr LocalFree(IntPtr hMem);
+
+        private byte[] StringVarBinaryToByteArray(string varBinaryData)
+        {
+            var byteList = new List<byte>();
+
+            string hexPart = varBinaryData.Substring(2);
+            for (int i = 0; i < hexPart.Length / 2; i++)
+            {
+                string hexNumber = hexPart.Substring(i * 2, 2);
+                byteList.Add((byte)Convert.ToInt32(hexNumber, 16));
+            }
+
+            return byteList.ToArray();
+        }
+
+        private string ByteArrayToVarBinaryString(byte[] bytes)
+        {
+            return "0x" + BitConverter.ToString(bytes).Replace("-", string.Empty);
+        }
+
+        private string ConvertBytesToStringSid(byte[] sidBytes)
+        {
+            short sSubAuthorityCount = 0;
+            StringBuilder strSid = new StringBuilder();
+            strSid.Append("S-");
+            try
+            {
+                // Add SID revision.
+                strSid.Append(sidBytes[0].ToString());
+
+                sSubAuthorityCount = Convert.ToInt16(sidBytes[1]);
+
+                // Next six bytes are SID authority value.
+                if (sidBytes[2] != 0 || sidBytes[3] != 0)
+                {
+                    string strAuth = String.Format("0x{0:2x}{1:2x}{2:2x}{3:2x}{4:2x}{5:2x}",
+                                   (Int16)sidBytes[2],
+                                   (Int16)sidBytes[3],
+                                   (Int16)sidBytes[4],
+                                   (Int16)sidBytes[5],
+                                   (Int16)sidBytes[6],
+                                   (Int16)sidBytes[7]);
+                    strSid.Append("-");
+                    strSid.Append(strAuth);
+                }
+                else
+                {
+                    Int64 iVal = sidBytes[7] +
+                         (sidBytes[6] << 8) +
+                         (sidBytes[5] << 16) +
+                         (sidBytes[4] << 24);
+                    strSid.Append("-");
+                    strSid.Append(iVal.ToString());
+                }
+
+                // Get sub authority count...
+                int idxAuth = 0;
+                for (int i = 0; i < sSubAuthorityCount; i++)
+                {
+                    idxAuth = 8 + i * 4;
+                    UInt32 iSubAuth = BitConverter.ToUInt32(sidBytes, idxAuth);
+                    strSid.Append("-");
+                    strSid.Append(iSubAuth.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return strSid.ToString();
+        }
+    }
+}
